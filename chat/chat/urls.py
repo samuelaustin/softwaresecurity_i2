d682c83f7cbf34c0090ad7f9298289ec7a821fa2
samuelaustin <samuelaustin_3@hotmail.com>
2015-12-18 15:33:36 +0100

    Database settings

--- chat/chat/urls.py
+++ chat/chat/urls.py
@@ -4,14 +4,9 @@ from django.conf.urls import patterns, include, url
 # from django.contrib import admin
 # admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'chat.views.home', name='home'),
-    # url(r'^chat/', include('chat.foo.urls')),
+from django.conf.urls import include, url
+from django.contrib import admin
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
-)
+urlpatterns = [
+    url(r'^chatApp/', include('chatApp.urls')),
+]
