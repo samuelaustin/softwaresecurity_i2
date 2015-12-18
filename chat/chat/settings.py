ad8a2f8ca09009e193ec35e6a23ef2c9bca10a01
Tim van Rossum <trvanrossum@gmail.com>
2015-12-18 16:22:25 +0100

    glhf

--- chat/chat/settings.py
+++ chat/chat/settings.py
@@ -114,7 +114,8 @@ TEMPLATE_DIRS = (
 )
 
 INSTALLED_APPS = (
-    'chat.app.chatApp',
+    'chatApp',
+    'chat',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
