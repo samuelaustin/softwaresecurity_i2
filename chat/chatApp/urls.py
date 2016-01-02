5c062c71698061dcda0b9f013e43abf9c4ee9699
samuelaustin <samuelaustin_3@hotmail.com>
2016-01-02 15:42:01 +0100

    Admin page

--- chat/chatApp/urls.py
+++ chat/chatApp/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
 from . import views
+from django.contrib import admin
 
 urlpatterns = [
 	url(r'^register/$', views.Register),
     url(r'^login/$', views.Login),
     url(r'^logout/$', views.Logout),
     url(r'^$', views.index, name='index'),
+    url(r'^admin/', admin.site.urls),
 ]
